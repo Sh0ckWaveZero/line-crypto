570fb5e758c7c1a9c19a76f2cb8dcf49945470f2
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-03-18 13:13:19 +0700

    Add support for GPT-3.5-turbo model and sanitize and deduplicate messages in OpenAI service

--- src/common/providers/openai.service.ts
+++ src/common/providers/openai.service.ts
@@ -7,24 +7,40 @@ import { Injectable } from '@nestjs/common';
 export class OpenAiService {
   private configuration: Configuration;
   private openai: OpenAIApi;
+  private model: string = 'gpt-3.5-turbo';
   constructor(
-    private readonly config: ConfigService
+    private readonly config: ConfigService,
   ) {
     this.configuration = new Configuration({
       apiKey: this.config.get('openAi.apiKey'),
     });
     this.openai = new OpenAIApi(this.configuration);
   }
+  private sanitizeMessage(message: string) {
+    return message.trim().replace(/[\n\r]/g, '').replace(/(\w)\.$/, '$1');
+  }
+  private deduplicateMessages = (array: string[]) => Array.from(new Set(array));
+
   public async getCompletion(prompt: string): Promise<any> {
-    const { data } = await this.openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt: prompt,
-      temperature: 0.8,
-      max_tokens: 500,
+    const { data }: any = await this.openai.createChatCompletion({
+      model: this.model,
+      messages: [{
+        role: 'user',
+        content: prompt,
+      }],
+      temperature: 0.7,
       top_p: 1.0,
-      frequency_penalty: 0.5,
+      frequency_penalty: 0,
       presence_penalty: 0,
+      max_tokens: 200,
+      n: 1,
     });
-    return data.choices[0].text;
+
+    const res = this.deduplicateMessages(
+      data.choices
+        .filter((choice: any) => choice.message?.content)
+        .map((choice: any) => this.sanitizeMessage(choice.message!.content))
+    );
+    return res[0];
   }
 }
